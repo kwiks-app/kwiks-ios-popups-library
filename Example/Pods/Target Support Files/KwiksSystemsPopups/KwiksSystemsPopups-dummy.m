fcbf9e5153aaf47b543d8d0ed945f1689dab204d
Eric Solberg <eric.jacob.solberg@gmail.com>
2023-12-14 16:10:55 -0700

    Removed cocoapods and added SPM

--- Example/Pods/Target Support Files/KwiksSystemsPopups/KwiksSystemsPopups-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_KwiksSystemsPopups : NSObject
-@end
-@implementation PodsDummy_KwiksSystemsPopups
-@end
--- Example/Pods/Target Support Files/Pods-KwiksSystemsPopups_Tests/Pods-KwiksSystemsPopups_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_KwiksSystemsPopups_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_KwiksSystemsPopups_Tests
-@end
