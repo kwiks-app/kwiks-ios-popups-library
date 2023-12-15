c868031f1b92a6523efa2dfa3fb82c74fe4f72fe
Eric Solberg <eric.jacob.solberg@gmail.com>
2023-12-15 01:19:32 -0700

    Fixed package.swift

--- Package.swift
+++ Package.swift
@@ -15,11 +15,7 @@ let package = Package(
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "kwiks-ios-popups-library",
-            dependencies: [],
-            resources: [
-                .process("Assets")
-            ]),
+            name: "kwiks-ios-popups-library"),
         .testTarget(
             name: "KwiksPopupsTests",
             dependencies: ["kwiks-ios-popups-library"]),
